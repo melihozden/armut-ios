76d05c8ad7583e62265575fbdbb1206513e75128
melihozden <melihozden403@gmail.com>
2020-09-04 15:17:29 +0300

    Initial Commit

---
+++ armutProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  armutProject
+//
+//  Created by Melih ÖZDEN on 4.09.2020.
+//  Copyright © 2020 Melih ÖZDEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
